59be1e313956fe6db023ca952e64108c23b7ebb7
Theo-Platt <theop@nmsu.edu>
2025-04-07 15:49:03 -0600

    Finished the experiment. Websocket connections work. HTTP POST requests work. HTTP GET requests do not work.

--- Networking Experiment - theo/godot/test.gd
+++ Networking Experiment - theo/godot/test.gd
@@ -2,74 +2,110 @@ extends Node2D
 var tcp_client = StreamPeerTCP.new()
 var server_address = "127.0.0.1"
 var server_port = 3500
+var first_connect = true
+var HTTP_CODE = {
+	100: "Continue",
+	200: "OK",
+	300: "Redirect",
+	400: "Client Error",
+	500: "Internal Server Error"
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var res = tcp_client.connect_to_host(server_address, server_port)
-	
-	if res == OK:
-		print("Connecting...")
-		await wait_for_connection()
-	else:
-		print("Failed to connect")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func tcp_receive(data):
-	print("Received: " + data)
-	if data.type == "server_message":
-		if data.command == "update_button":
-			$textField.text = data.data
+
+
+
+
+func tcp_receive(bytes):
+	if tcp_client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
+		var data = tcp_client.get_data(bytes)
+		if data[0]!=0: 
+			print("Error in reading data")
+		else:
+			var response = data[1].get_string_from_utf8()
+			var json = JSON.parse_string(response)
+			print("Websocket Data Received")
+			print("  Code:     "+str(json["code"])+" "+HTTP_CODE[200])
+			print("  Response: "+str(json["response"]))
+			print()
+
+	else:
+		print("Not connected.")
+
+
 
 func tcp_send(data) -> void:
 	if tcp_client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
 		var message = str(data) + "\n"
 		var buffer = message.to_utf8_buffer()
-		print("Send: ", message)
-		var sent = tcp_client.put_data(buffer)
+		tcp_client.put_data(buffer)
 	else:
 		print("Not connected.")
-	
-	
+
+
+
+
+func tcp_connect():
+	var res = tcp_client.connect_to_host(server_address, server_port)
+	if res == OK:
+		if first_connect: 
+			print("Connecting...")
+		else:
+			print("Attempting to reconnect...")
+		await wait_for_connection()
+		
+		
+	else:
+		print("Failed to connect")
+
 func wait_for_connection() -> void:
-	var timeout = 20.0  # seconds
+	var timeout = 5.0  # seconds
 	var elapsed = 0.0
 	while tcp_client.get_status() != StreamPeerTCP.STATUS_CONNECTED and elapsed < timeout:
 		await get_tree().create_timer(0.1).timeout
 		elapsed += 0.1
-		tcp_client.poll()
 	
 	if tcp_client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
 		print("Connected.")
+		first_connect=false
 	else:
 		print("Connection timed out.")
-	
-# the bar button
-func _on_button_2_pressed() -> void:
-	print("Button Pressed: BAR")
-	tcp_send("BAR")
+		tcp_client.disconnect_from_host()
+		await get_tree().create_timer(10).timeout
+	$Network_Timer.start()
 
 
-# the foo button
-func _on_button_pressed() -> void:
-	print("Button Pressed: FOO")
-	tcp_send("FOO")
+
 
 
 func _on_timer_timeout() -> void:
-	print("timer!")
+	tcp_client.poll()
 	var status = tcp_client.get_status()
 	if status == StreamPeerTCP.STATUS_CONNECTED:
-		if tcp_client.get_available_bytes() > 0:
-			var data = tcp_client.get_string()
-			print(data)
-			var json_data = JSON.parse_string(data)
-			if json_data:
-				print("Received from server:", json_data)
-	else:
-		print("Not Connected.")
+		var bytes_available = tcp_client.get_available_bytes()
+		if bytes_available > 0:
+			tcp_receive(bytes_available)
+	elif status == StreamPeerTCP.STATUS_NONE:
+		tcp_connect()
 
+
+
+
+
+func _on_button_2_pressed() -> void:
+	print("Button Pressed: FOO")
+	tcp_send("foobar")
+
+func _on_button_pressed() -> void:
+	
+	print("Button Pressed: BAR")
+	tcp_send({"Foo":{"bar":42}})
 
 # Note: This test is run on Godot 4.3
