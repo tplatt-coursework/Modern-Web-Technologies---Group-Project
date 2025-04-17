f2f4c28e1167b3ddfd44ff7b3f1009133e7c5267
Xindi <xindizheng123@gmail.com>
2025-04-16 21:44:19 -0600

    two boxes not messing with each other, and jump() worked

--- game-project/code_box.gd
+++ game-project/code_box.gd
@@ -1,5 +1,23 @@
 extends Node
 
+var player: Node = null
+
+#just to find the player node....
+func _recursive_find_node(node: Node, target_name: String) -> Node:
+	if node.name == target_name:
+		return node
+	for child in node.get_children():
+		var found = _recursive_find_node(child, target_name)
+		if found:
+			return found
+	return null
+
+func _ready():
+	var root = get_tree().get_root()
+	var gamebox = _recursive_find_node(root, "GameBox")
+	if gamebox:
+		player = gamebox.get_node_or_null("player")
+
 func _on_run_button_pressed() -> void:
 	var user_code = $CodeEdit.text.split("\n")
 	for line in user_code:
@@ -9,6 +27,9 @@ func _on_run_button_pressed() -> void:
 func _parse_and_execute(line: String):
 	match line:
 		"jump()":
+			#tried with player jump	
+			if player:
+				player.jump()
 			print("jumping")
 			# call  jump function here
 		"move_left()":
